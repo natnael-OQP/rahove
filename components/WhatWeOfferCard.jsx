56cecc0775b9e1f493830876d20b9c2591398498
natnael-OQP <makiya112545@gmail.com>
2022-08-19 11:59:32 +0300

    add icons to my app

---
+++ components/WhatWeOfferCard.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import React from 'react'
+
+function WhatWeOfferCard() {
+    return (
+        <div>
+            <div>
+                <Image
+                    layout="fill"
+                    objectFit="cover"
+                    src="/assets/images/bg2.png"
+                    alt="logo"
+                />
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default WhatWeOfferCard
