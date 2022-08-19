fec80631aa1624185a64d8b5b81bc71d25097861
natnael-OQP <makiya112545@gmail.com>
2022-08-19 13:57:48 +0300

    style completed

--- components/WhatWeOfferCard.jsx
+++ components/WhatWeOfferCard.jsx
@@ -1,19 +1,20 @@
 import Image from 'next/image'
 import React from 'react'
 
-function WhatWeOfferCard() {
+function WhatWeOfferCard({ icon, title, description }) {
     return (
-        <div>
-            <div>
+        <div className="flex p-5  bg-white z-50">
+            <div className="w-28 h-12 mt-3  relative ">
                 <Image
                     layout="fill"
-                    objectFit="cover"
-                    src="/assets/images/bg2.png"
-                    alt="logo"
+                    objectFit="contain"
+                    src={icon}
+                    alt={title}
                 />
             </div>
-            <div>
-
+            <div className="flex flex-col ml-5">
+                <h1 className="text-black text-sm  font-medium ">{title}</h1>
+                <p className="text-gray-600 text-xs font-normal mt-3" >{description}</p>
             </div>
         </div>
     )
